7b23d6aa2538a52d0014bc757df019a6d2294a02
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2025-02-14 23:32:01 +0500

    set tracking

--- src/helper/shipEngine.ts
+++ src/helper/shipEngine.ts
@@ -11,6 +11,7 @@ import ShipEngine from "shipengine";
 
 console.log("Loaded API Key:", process.env.SHIPENGINE_API_KEY); // Debugging log
 
+
 if (!process.env.SHIPENGINE_API_KEY) {
   throw new Error("ShipEngine API Key is missing! Check your .env.local file.");
 }
